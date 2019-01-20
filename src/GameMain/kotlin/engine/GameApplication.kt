bc8d0e3921c38456b2cfe5b876260229deb95434
felipehjcosta <felipe.h.j.costa@gmail.com>
2019-01-19 22:44:13 -0200

    Create a simple game loop

--- src/GameMain/kotlin/engine/GameApplication.kt
+++ src/GameMain/kotlin/engine/GameApplication.kt
@@ -1,13 +1,14 @@
 package engine
 
 import engine.io.printErr
-import kotlinx.cinterop.pointed
+import kotlinx.cinterop.*
 import sdl.*
 
 class GameApplication {
 
     private var window: kotlinx.cinterop.CPointer<cnames.structs.SDL_Window>? = null
     private var windowSurface: kotlinx.cinterop.CPointer<sdl.SDL_Surface>? = null
+    private var isQuitting = false
 
     fun initInstance(): Boolean {
         if (SDL_Init(SDL_INIT_VIDEO) < 0) {
@@ -34,6 +35,22 @@ class GameApplication {
     fun run() {
         SDL_FillRect(windowSurface, null, SDL_MapRGB(windowSurface?.pointed?.format, 0xFF, 0xFF, 0xFF))
         SDL_UpdateWindowSurface(window)
-        SDL_Delay(4000.toUInt())
+
+        var isRunning = true
+
+        while (isRunning) {
+            if (isQuitting) {
+                isRunning = false
+            }
+
+            memScoped {
+                val event = alloc<SDL_Event>()
+                if (SDL_PollEvent(event.ptr.reinterpret()) != 0) {
+                    when (event.type) {
+                        SDL_QUIT -> isQuitting = true
+                    }
+                }
+            }
+        }
     }
 }
